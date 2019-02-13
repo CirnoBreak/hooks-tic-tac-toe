d31f4aeb2b074dbc8e929171b3a2e364003e41d3
CirnoBreak <1103054869@qq.com>
2019-02-14 01:36:40 +0800

    state hoisting

--- src/Board.js
+++ src/Board.js
@@ -1,28 +1,92 @@
-import React, { useState } from 'react';
-import RenderSquare from './RenderSquare';
+import React, { useReducer } from 'react';
+import Square from './Square';
+
+const initState = {
+  squares: Array(9).fill(null),
+  xIsNext: true
+};
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'changeSquares':
+      const { squares, xIsNext } = state;
+      const { index }= action;
+      const newSquares = squares.slice();
+      newSquares[index] = xIsNext ? 'X' : 'O';
+      return {
+        ...state,
+        squares: newSquares,
+        xIsNext: !xIsNext
+      }
+    default:
+      return state;
+  }
+}
+
+function RenderSquare ({ value, onClick }) {
+  return (
+    <Square
+      value={value}
+      onClick={onClick}
+    />
+  )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+function genSquare(start, squares, dispatch) {
+  return (
+    <div className="board-row">
+      {
+        Array.from({ length: 3}).map((val, index, arr) => (
+          <RenderSquare
+            key={index}
+            value={squares[start + index]}
+            onClick={() => dispatch({type: 'changeSquares', index: start + index})}
+          />
+        ))
+      }
+    </div>
+  )
+}
 
 function Board () {
-  const status = 'Next player: X';
-  const [xIsNext, setXIsNext] = useState(true);
+  const [state, dispatch] = useReducer(reducer, initState);
+  const { squares, xIsNext } = state;
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next Player: ' + (xIsNext ? 'X' : 'O');
+  }
   return (
     <div>
-      <div className="status">{ status }</div>
-      <div className="board-row">
-        <RenderSquare index="0" xIsNext={xIsNext} onClick={() => setXIsNext(!xIsNext)} />
-        <RenderSquare index="1" xIsNext={xIsNext} onClick={() => setXIsNext(!xIsNext)} />
-        <RenderSquare index="2" xIsNext={xIsNext} onClick={() => setXIsNext(!xIsNext)} />
+      <div className="status">
+        { status }
       </div>
-      <div className="board-row">
-        <RenderSquare index="3" xIsNext={xIsNext} onClick={() => setXIsNext(!xIsNext)} />
-        <RenderSquare index="4" xIsNext={xIsNext} onClick={() => setXIsNext(!xIsNext)} />
-        <RenderSquare index="5" xIsNext={xIsNext} onClick={() => setXIsNext(!xIsNext)} />
-      </div>
-      <div className="board-row">
-        <RenderSquare index="6" xIsNext={xIsNext} onClick={() => setXIsNext(!xIsNext)} />
-        <RenderSquare index="7" xIsNext={xIsNext} onClick={() => setXIsNext(!xIsNext)} />
-        <RenderSquare index="8" xIsNext={xIsNext} onClick={() => setXIsNext(!xIsNext)} />
-        
-      </div>
+      { genSquare(0, squares, dispatch) }
+      { genSquare(3, squares, dispatch) }
+      { genSquare(6, squares, dispatch) }
     </div>
   )
 }
